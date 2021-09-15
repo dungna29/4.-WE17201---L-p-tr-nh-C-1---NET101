3fe675717da4b8cc1dd4b9cd1402ea0974687395
Dungna89 <dungna29@fpt.edu.vn>
2021-09-15 18:25:29 +0700

    1

---
+++ 4_WE17201_NET101_FALL21/BAI_1.2_BienLaGi/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace BAI_1._2_BienLaGi
+{
+    class Program
+    {
+        /*
+         * Bài 1.1 Khai báo biến
+         * Định nghĩa biến: Dùng để lưu trữ giá trị hoặc 1 tập giá trị và biến
+         * được khở tạo trong bộ nhớ ảo khi chương trình khởi chạy.
+         *
+         *  CÔNG THỨC: <Kiểu dữ liệu> <Tên Biến> = <Giá trị>;
+         *
+         *  1. <Kiểu dữ liệu>: Giúp định nghĩa kiểu dữ liệu cho biến cần lưu trữ giá trị gì.
+                        + Số nguyên:sbyte, byte, long, int
+                        + Sô thực: float, double, decimal
+                        + Chuỗi hoặc ký tự: string, char
+                        + Logic: bool
+                        + var: nó có thể đại diện là tất cả các kiểu dữ liệu.
+         *
+         *  2. <Tên Biến>: Định danh biến giúp phân biệt giữa các biến trong 1 file.     Lam
+         *      + Quy tắc đặt tên:
+         *          - Tên biến phải có nghĩa
+         *          - Biến có phân biệt chữ hoa chữ thường
+         *          - Không bắt đầu bởi số, không dùng từ khóa
+         *          - Cố gắng làm quen đặt tên biến với Tiếng Anh.
+               + Khai báo tên biến với 2 từ trở lên:
+                 - Cách 1: normal = firstname
+                 - Cách 2: CamelCase = FirstName, firstName
+                 - Cách 3: Under_Score = first_name, first_Name
+
+         *     + Biến cục bộ: là 1 biến được khai báo bên trong 1 phương thức. Không thể gọi biến cục bộ sang 1 phương thức khác.
+         *     + Biến toàn cục: là 1 biến được khai báo bên ngoài phương thức . Các phương thức có thể gọi đến nó.
+         *              - Khi khai báo biến toàn cục sử dụng dấu gạch dưới _ trước tên biến. Biến toàn cục luôn đặt ở trên đầu của Class.
+         *              - Ví dụ: int _year = 1989;
+         *
+         * 3. <Giá trị>:
+         *  + Khi khai báo biến có thể khởi tạo giá trị hoặc không khởi tạo giá trị.
+         *  + Giá trị phải đúng với quy tắc của kiểu dữ liệu;
+         */
+
+        private int _year;
+        static void Main(string[] args)
+        {
+          //1. Chuỗi
+          string name1;//Khởi tạo 1 biến tên name1 có kiểu dữ liệu là string và không khởi tạo giá trị ban đầu.
+          string name2 = "FPOLY";//Khởi tạo 1 biến tên name2 có kiểu dữ liệu là string và có khởi tạo giá trị ban đầu.
+          string name3, name4, name5 = "C#1";//Khai báo nhiều biến trên cùng một dòng miễn có cùng kiểu dữ liệu.
+
+          //2. Logic
+          bool logic1 = true;
+          bool logic2 = false;
+
+          //3. var - Kiểu dữ liệu ngầm định
+          //var v0;//Phải khởi tạo giá trị ngay khi sử dụng
+          var v1 = 1;//int
+          var v2 = "1";//string
+          var v3 = '1';//char
+          var v4 = 5.6;//double
+          var v5 = true;//bool
+        }
+    }
+}
---
+++ 4_WE17201_NET101_FALL21/Bai_1.3_NhapXuatTuBanPhim/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai_1._3_NhapXuatTuBanPhim
+{
+    class Program
+    {
+        /*
+         * BÀI 1.23 Nhập xuất dữ liệu từ bàn phím
+         *  1. Xuất dữ liệu ra màn hình
+         *      + Phím tắt : cw + tab
+         *      + Console.WriteLine(); Ghi xong thì xuống dòng
+         *      + Console.Write(); Ghi xong thì không xuống dòng
+         *  2. Nhập dữ liệu từ bàn phím:
+         *      + Console.Read(); Đọc 1 ký tự từ bàn phím nhưng là số ASII
+         *      + Console.ReadLine();  Đọc nhiều ký tự từ bàn phím và kiểu dữ liệu là string - chuỗi
+         */
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.GetEncoding("UTF-8");
+            //Phần 1: Xuất dữ liệu ra màn hình
+            Console.Write("Chào các bạn ");
+            Console.WriteLine("học môn C#");
+            Console.Write(" tại \n FPOLY \n");
+            //Ctrl + F5 chạy chương trình
+
+            //Tạo template mẫu
+            Console.WriteLine("Chào bạn: {0} Năm Sinh: {1} Trường: {2}","Hương",2000, "Fpoly");
+
+            Console.WriteLine("Chào bạn: Hương Năm Sinh: 2000 Trường: Fpoly");
+
+            //Phần 2: Nhập dữ liệu từ bàn phím
+            //Viết 1 chương trình nhập tên và in ra màn hình
+            string name;
+            Console.Write("Mời bạn nhập tên: ");//Chỉ dẫn cho người dùng cần phải làm gì
+            name = Console.ReadLine();//Gán giá trị do người dùng nhập vào Console.ReadLine() có kiểu dữ liệu là string
+            Console.WriteLine("Chào bạn " + name);//Hành động cuối in ra màn hình
+
+        }
+    }
+}
